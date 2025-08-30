345c3812fd01af8af6c0c78939969fd51e394300
Shrimant Shaw <161811713+Shrimant05@users.noreply.github.com>
2025-08-30 18:26:52 +0530

    APP UI Complete

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants/index";
+import Resumecard from "~/components/Resumecard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main>
+      <section className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar></Navbar>
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submission and check AI-powered feedback</h2>
+        </div>
+      
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <Resumecard  key={resume.id} resume={resume}></Resumecard>
+          ))}
+        </div>
+      )}
+      </section>
+    </main>
+  );
 }
